1f44eef076d0c0c027cad4637366df69b21bb110
Thomas Maloney <thomasjmaloney99@gmail.com>
2026-01-07 23:48:14 -0600

    Add duration info to bpm node

--- src/file_reading/parser/bpm_node.cpp
+++ src/file_reading/parser/bpm_node.cpp
@@ -2,11 +2,14 @@
 #include "file_reading/parser/node.hpp"
 
 namespace FileReading::Parser {
-BpmNode::BpmNode(FileReading::Lexer::Token *token, unsigned int bpm)
-    : Node(token), _bpm(bpm) {}
+BpmNode::BpmNode(FileReading::Lexer::Token *token, unsigned int bpm,
+                 DurationNode *duration)
+    : Node(token), _bpm(bpm), _duration(duration) {}
 
 NodeKind BpmNode::kind() const { return NodeKind::Bpm_Decl; }
 
 unsigned int BpmNode::bpm() const { return _bpm; }
+
+DurationNode *BpmNode::duration() const { return _duration; }
 
 } // namespace FileReading::Parser
